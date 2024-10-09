92362f7114dae03be2dd177de978d8385ba29955
clintcc <clintcconklin@gmail.com>
2024-10-09 14:41:47 -0600

    Testing

--- GridGeneration/Assets/_Scripts/LevelBuilder.cs
+++ GridGeneration/Assets/_Scripts/LevelBuilder.cs
@@ -1,41 +1,49 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using _Scripts;
 using _Scripts.Extensions.ScriptableObjectExtensions;
 using UnityEngine;
 using Bogus;
-using Random = UnityEngine.Random;
-
+using JetBrains.Annotations;
 
 public class LevelBuilder : MonoBehaviour
 {
-    public GameObject groundTilePrefab;
-    public GameObject obstaclePrefab;
-    public int width = 10;
-    public int height = 10;
-    public LevelDataSO levelDataSO;
-
+    private const float CameraZPosition = -10f;
+    private const float ObstacleProbability = 0.3f;
+    private const string TileNameFormat = "TILE [{0},{1}]";
+    private const string LevelDataFilePath = "/levelData.json";
+    private LevelDataSO levelDataSO;
     private string savePath;
 
+    [SerializeField]
+    private GameObject groundTilePrefab;
+    [SerializeField]
+    private GameObject obstaclePrefab;
+    [SerializeField]
+    private int width = 16;
+    [SerializeField]
+    private int height = 9;
+
+    /// <summary>
+    ///     Initializes the level builder.
+    /// </summary>
     void Start()
     {
-        savePath = Application.persistentDataPath + "/levelData.json";
+        savePath = Application.persistentDataPath + LevelDataFilePath;
         LoadLevel();
         CenterCameraOnLevel();
     }
 
+    /// <summary>
+    ///     Generates a new level.
+    /// </summary>
+    /// <remarks>
+    ///     The level is generated using a faker to generate tile data.
+    /// </remarks>
     public void GenerateLevel()
     {
-        levelDataSO = ScriptableObject.CreateInstance<LevelDataSO>();
-        levelDataSO.width = width;
-        levelDataSO.height = height;
-
-        var levelDataFaker = new Faker<LevelDataSO.TileData>()
-            .RuleFor(td => td.hasObstacle, f => f.Random.Bool(0.3f))
-            .RuleFor(td => td.levelTileType, f => f.PickRandom<LevelTileType>());
-
-        levelDataSO.tiles = new List<LevelDataSO.TileData>();
+        InitializeLevelData();
+        var levelDataFaker = CreateTileDataFaker();
 
         for (int x = 0; x < width; x++)
         {
@@ -45,67 +53,131 @@ public class LevelBuilder : MonoBehaviour
                 tileData.position = new Vector2Int(x, y);
                 levelDataSO.tiles.Add(tileData);
 
-                Vector3 position = new Vector3(x, y, 0);
-                var instance = tileData.hasObstacle ? obstaclePrefab : groundTilePrefab;
-                var tileInstance = Instantiate(instance, position, Quaternion.identity);
-                tileInstance.name = $"TILE [{x},{y}]";
+                InstantiateTile(tileData, x, y);
             }
         }
     }
 
+    /// <summary>
+    ///     Saves the generated level to a file.
+    /// </summary>
     public void SaveGeneratedLevel()
     {
         ScriptableObjectSerializer<LevelDataSO>.SaveToFile(levelDataSO, savePath);
-        Debug.Log("Level Saved: " + savePath);
+        Debug.Log($"Level Saved: {savePath}");
     }
 
+    /// <summary>
+    ///     Loads the level from a file.
+    /// </summary>
     public void LoadLevel()
     {
         if (File.Exists(savePath))
         {
             levelDataSO = ScriptableObjectSerializer<LevelDataSO>.LoadFromFile(savePath);
-            Debug.Log("Level Loaded: " + savePath);
-
-            // Recreate level from loaded data
-            foreach (var tileData in levelDataSO.tiles)
-            {
-                Vector3 position = new Vector3(x: tileData.position.x, z: 0, y: tileData.position.y);
-                var instance = tileData.hasObstacle ? obstaclePrefab : groundTilePrefab;
-                var tileInstance = Instantiate(instance, position, Quaternion.identity);
-                tileInstance.name = $"TILE [{tileData.position.x},{tileData.position.y}]";
-            }
+            Debug.Log($"Level Loaded: {savePath}");
+            RecreateLoadedLevel();
         }
         else
         {
             Debug.Log("No saved level found. Generating a new level...");
-            GenerateLevel();
-            SaveGeneratedLevel();  // Save the newly generated level for future use
+            GenerateAndSaveLevel();
         }
     }
-    
+
+    /// <summary>
+    ///     Regenerates the level.
+    /// </summary>
     public void RegenerateLevel()
     {
+        Debug.Log("Regenerating level...");
         foreach (Transform child in transform)
         {
             Destroy(child.gameObject);
         }
         GenerateLevel();
     }
-    
+
+    /// <summary>
+    ///     Centers the camera on the level.
+    /// </summary>
     private void CenterCameraOnLevel()
     {
         Camera mainCamera = Camera.main;
         if (mainCamera != null)
         {
-            // Calculate the center of the level
-            var centerX = (width - 1) / 2.0f;
-            var centerY = (height - 1) / 2.0f;
-
-            // Set camera position to center above the level
-            Vector3 centerPosition = new Vector3(centerX, centerY, -10f); // Adjust y value for height above level
+            Vector3 centerPosition = new Vector3((width - 1) / 2.0f, (height - 1) / 2.0f, CameraZPosition);
             mainCamera.transform.position = centerPosition;
         }
     }
+
+    /// <summary>
+    ///     Initializes the level data.
+    /// </summary>
+    private void InitializeLevelData()
+    {
+        levelDataSO = ScriptableObject.CreateInstance<LevelDataSO>();
+        levelDataSO.width = width;
+        levelDataSO.height = height;
+        levelDataSO.tiles = new List<LevelDataSO.TileData>();
+    }
+
+    /// <summary>
+    ///     Creates a faker for generating tile data.
+    /// </summary>
+    /// <returns>
+    ///     A faker for generating tile data.
+    /// </returns>
+    private Faker<LevelDataSO.TileData> CreateTileDataFaker()
+    {
+        return new Faker<LevelDataSO.TileData>()
+            .RuleFor(td => td.hasObstacle, f => f.Random.Bool(ObstacleProbability))
+            .RuleFor(td => td.levelTileType, f => f.PickRandom<LevelTileType>());
+    }
+
+    
+    /// <summary>
+    ///     Instantiates a tile at the given position.
+    /// </summary>
+    /// <param name="tileData">
+    ///     The tile data to use for instantiation.
+    /// </param>
+    /// <param name="x">
+    ///     The x position of the tile.
+    /// </param>
+    /// <param name="y">
+    ///     The y position of the tile.
+    /// </param>
+    private void InstantiateTile(LevelDataSO.TileData tileData, int x, int y)
+    {
+        Vector3 position = new Vector3(x, y, 0);
+        GameObject instance = tileData.hasObstacle ? obstaclePrefab : groundTilePrefab;
+        GameObject tileInstance = Instantiate(instance, position, Quaternion.identity);
+        tileInstance.name = string.Format(TileNameFormat, x, y);
+        
+    }
+
+    /// <summary>
+    ///     Recreates the level from the loaded level data.
+    /// </summary>
+    private void RecreateLoadedLevel()
+    {
+        foreach (LevelDataSO.TileData tileData in levelDataSO.tiles)
+        {
+            InstantiateTile(tileData, tileData.position.x, tileData.position.y);
+        }
+    }
+
+    /// <summary>
+    ///     Generates a new level and saves it to a file.
+    /// </summary>
+    private void GenerateAndSaveLevel()
+    {
+        GenerateLevel();
+        SaveGeneratedLevel();
+    }
+}
+
     
     // private void CreateLevelData()
     // {
@@ -153,4 +225,3 @@ public class LevelBuilder : MonoBehaviour
     //         }
     //     }
     // }
-}
